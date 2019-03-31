d1e8b69d2a11a66cb5561b7a62d00cb26bb2efbc
simonguozirui <simonguozirui@gmail.com>
2019-03-31 01:51:42 -0700

    osc client simulator completed

---
+++ Visualization/osc-client-sample.py
@@ -0,0 +1,20 @@
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+import argparse
+import random
+import time
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--ip", default="127.0.0.1",
+      help="The ip of the OSC server")
+  parser.add_argument("--port", type=int, default=5005,
+      help="The port the OSC server is listening on")
+  args = parser.parse_args()
+
+  client = udp_client.SimpleUDPClient(args.ip, args.port)
+
+  for x in range(10):
+    client.send_message("/filter", random.random())
+    time.sleep(1)
--- Visualization/osc-simulator.py
+++ Visualization/osc-simulator.py
@@ -2,19 +2,33 @@ from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
 import argparse
-import random
+from random import randint
 import time
+import sys
 
-if __name__ == "__main__":
-  parser = argparse.ArgumentParser()
-  parser.add_argument("--ip", default="127.0.0.1",
-      help="The ip of the OSC server")
-  parser.add_argument("--port", type=int, default=5005,
-      help="The port the OSC server is listening on")
-  args = parser.parse_args()
+portNum = 5005
+ipNum = "127.0.0.1"
+instrument = sys.argv[1]
+client = udp_client.SimpleUDPClient(ipNum, portNum)
 
-  client = udp_client.SimpleUDPClient(args.ip, args.port)
+def guitarNotesSimulator():
+    while True:
+        client.send_message("/hit", randint(1, 16))
+        time.sleep(1)
+def drumNotesSimulator():
+    while True:
+        client.send_message("/hit", randint(1, 4))
+        time.sleep(1)
 
-  for x in range(10):
-    client.send_message("/filter", random.random())
-    time.sleep(1)
+if instrument == "guitar":
+    client.send_message("/instrument", "guitar")
+    guitarNotesSimulator()
+elif instrument == "drum":
+    client.send_message("/instrument", "drum")
+    drumNotesSimulator()
+else:
+    pass
+#
+# for x in range(10):
+#     client.send_message("/pass", random.random())
+#     time.sleep(1)
