23be576f5e8816273e1eb28cb0deb94e3a7b27b4
simonguozirui <simonguozirui@gmail.com>
2019-03-31 05:09:20 -0700

    print letter and take out unnecessary code

--- osc-web-environment/web-side/js/index.js
+++ osc-web-environment/web-side/js/index.js
@@ -4,30 +4,31 @@
 //
 //
 // // osc socket connection
-// var socket = io('http://127.0.0.1:8081');
-// console.log('oi');
-// socket.on('connect', function() {
-// 		 // sends to socket.io server the host/port of oscServer
-// 		 // and oscClient
-// 		 socket.emit('config',
-// 				 {
-// 						 server: {
-// 								 port: 3333,
-// 								 host: '127.0.0.1'
-// 						 },
-// 						 client: {
-// 								 port: 3334,
-// 								 host: '127.0.0.1'
-// 						 }
-// 				 }
-// 		 );
-//  });
-//
-//  socket.on('message', function(obj) {
-// 		 var status = document.getElementById("status");
-// 		 status.innerHTML = obj[0];
-// 		 console.log(obj);
-//  });
+var socket = io('http://127.0.0.1:8081');
+console.log('oi');
+socket.on('connect', function() {
+		 // sends to socket.io server the host/port of oscServer
+		 // and oscClient
+		 socket.emit('config',
+				 {
+						 server: {
+								 port: 3333,
+								 host: '127.0.0.1'
+						 },
+						 client: {
+								 port: 3334,
+								 host: '127.0.0.1'
+						 }
+				 }
+		 );
+ });
+
+ socket.on('message', function(obj) {
+		 var status = document.getElementById("status");
+		 status.innerHTML = obj[0];
+		 console.log(obj[1]);
+		 
+ });
 
 // crash varibles
 crashCymbolAll = document.getElementById('Crash');
